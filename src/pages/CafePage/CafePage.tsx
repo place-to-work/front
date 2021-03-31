a7e2091212003999441bb64a9417b320cb42e07e
qwer <aptyp.potatoch@gmail.com>
2021-03-31 19:43:41 +0300

    magic improving

--- src/pages/CafePage/CafePage.tsx
+++ src/pages/CafePage/CafePage.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import {observer} from 'mobx-react-lite';
 import DetailedInfo from '@components/DetailedInfo';
 import Header from '@components/a11y/Header';
-import { useParams } from 'react-router-dom';
 
-const CafePage: React.FC = () => {
-	const { id } = useParams();
-	return (
-		<div className="cafe-page">
-			<Header/>
-			{id}
-			<DetailedInfo />
-		</div>);
-};
+const CafePage: React.FC = () => (
+	<div className="cafe-page">
+		<Header/>
+		<DetailedInfo />
+	</div>);
 
 export default observer(CafePage);
