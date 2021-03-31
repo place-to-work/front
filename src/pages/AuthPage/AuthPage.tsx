9fe487d2777b61bac33411a85f976ac5503329a4
qwer <aptyp.potatoch@gmail.com>
2021-03-31 19:27:52 +0300

    redirect via useHistory done

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -7,56 +7,62 @@ import Button, {ButtonColor, ButtonSize} from '@components/primitives/Button';
 import Footer from '@components/a11y/Footer';
 import './AuthPage.scss';
 import PageContainer from '@components/a11y/PageContainer';
+import {useHistory} from 'react-router-dom';
 
-const AuthPage: React.FC = () => (<>
-	<PageContainer>
-		<Header style={{justifyContent: 'center', padding: 11}}>
-			<Typo
-				// element='a'
-				block
-				type={TypographyType.h3}
-				href="/"
-			>
-				Рабочее место
-			</Typo>
-		</Header>
-		<Main style={{padding: 10, width: 360}}>
-			<ImageCard
-				imageSrc={'https://p0.zoon.ru/b/0/4f85bd4b3c72dd81140000ef_5d1b13463ca0a.jpg'}
-				style={{width: '100%'}}
-			/>
-			<Typo block type={TypographyType.h1} style={{marginBottom: '18px', lineHeight: '1.1'}}>Сделайте город своим офисом!</Typo>
-			<Typo block type={TypographyType.h5} style={{marginBottom: '18px'}}>Превращаем кафе и музеи вашего города в настоящие рабочие пространства!</Typo>
-			<Button
-				element="a"
-				href="/signup"
-				full
-				buttonSize={ButtonSize.classic}
-				style={{marginBottom: '9px'}}
-			>
-				Регистрация
-			</Button>
-			<Button
-				element="a"
-				href="/login"
-				full
-				color={ButtonColor.accentGrey}
-			>
-				Вход
-			</Button>
-		</Main>
-		<Footer style={{flexDirection: 'row-reverse', padding: 8}}>
-			<Typo
-				// element="a"
-				href="/contact"
-				block
-				type={TypographyType.h5}
-				style={{marginRight: 'calc(50% - 170px)'}}
-			>
-				Свяжитесь с нами
-			</Typo>
-		</Footer>
-	</PageContainer>
-</>);
+const AuthPage: React.FC = () => {
+	const history = useHistory();
+	return <>
+		<PageContainer>
+			<Header style={{justifyContent: 'center', padding: 11}}>
+				<Typo
+					// element='a'
+					block
+					type={TypographyType.h3}
+					href="/"
+				>
+					Рабочее место
+				</Typo>
+			</Header>
+			<Main style={{padding: 10, width: 360}}>
+				<ImageCard
+					imageSrc={'https://p0.zoon.ru/b/0/4f85bd4b3c72dd81140000ef_5d1b13463ca0a.jpg'}
+					style={{width: '100%'}}
+				/>
+				<Typo block type={TypographyType.h1} style={{marginBottom: '18px', lineHeight: '1.1'}}>Сделайте
+					город своим офисом!</Typo>
+				<Typo block type={TypographyType.h5} style={{marginBottom: '18px'}}>Превращаем кафе и
+					музеи вашего города в настоящие рабочие пространства!</Typo>
+				<Button
+					element="a"
+					onClick={() => history.push('signup')}
+					full
+					buttonSize={ButtonSize.classic}
+					style={{marginBottom: '9px'}}
+				>
+					Регистрация
+				</Button>
+				<Button
+					element="a"
+					onClick={() => history.push('/login')}
+					full
+					color={ButtonColor.accentGrey}
+				>
+					Вход
+				</Button>
+			</Main>
+			<Footer style={{flexDirection: 'row-reverse', padding: 8}}>
+				<Typo
+					// element="a"
+					href="/contact"
+					block
+					type={TypographyType.h5}
+					style={{marginRight: 'calc(50% - 170px)'}}
+				>
+					Свяжитесь с нами
+				</Typo>
+			</Footer>
+		</PageContainer>
+	</>;
+};
 
 export default AuthPage;
