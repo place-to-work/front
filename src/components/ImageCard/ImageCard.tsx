5923fc853cb13d9ddd66d027f4bb7dc003463825
qwer <aptyp.potatoch@gmail.com>
2021-03-31 11:15:07 +0300

    image card: style tag added

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import './ImageCard.scss';
+import {CSSProperties} from 'react';
 type Props = {
     imageSrc?: string;
+    style?: CSSProperties;
 }
-const ImageCard: React.FC<Props> = ({imageSrc}:Props)=><img className="image" src={imageSrc} alt=""/>;
+const ImageCard: React.FC<Props> = ({imageSrc, style}:Props) => (
+	<img className="image" src={imageSrc} style={style} alt=""/>
+);
 export default ImageCard;
