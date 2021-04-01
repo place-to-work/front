f6960151061476ddfdaf40c2f4177b3b0ee6d9a4
qwer <aptyp.potatoch@gmail.com>
2021-04-02 02:32:12 +0300

    in-place screen added

--- src/pages/InWorkPlace/InWorkPlace.tsx
+++ src/pages/InWorkPlace/InWorkPlace.tsx
@@ -29,14 +29,29 @@ const InWorkPlace: React.FC<InWorkPlaceProps> = ({qrValue}) => {
 				</div>
 			</div>
 
-			<Typo block type={TypographyType.h1} textAlign={TypoTextAlign.center}>Подписка активирована</Typo>
-			<Typo block type={TypographyType.h3} textAlign={TypoTextAlign.center}>
+			<Typo
+				block
+				type={TypographyType.h1}
+				textAlign={TypoTextAlign.center}
+				style={{lineHeight: 1, margin: '14px 0'}}
+			>
+				Подписка активирована
+			</Typo>
+			<Typo
+				block
+				type={TypographyType.h4}
+				textAlign={TypoTextAlign.center}
+				style={{marginBottom: 14, width: 320}}
+			>
 				Предъявите этот код баристе и получайте
 				бесплатный чай, скидки в кафе и
 				неограниченный доступ рабочие пространства
 			</Typo>
-			<Typo type={TypographyType.h3}>Дата истекания: </Typo>
-			<Typo type={TypographyType.h3} weight={TypoWeight.bold}>26 апреля</Typo>
+
+			<div style={{display: 'flex', justifyContent: 'center', marginBottom: 16}}>
+				<Typo type={TypographyType.h4}>Дата истекания: <Typo type={TypographyType.h4} weight={TypoWeight.bold}>26 апреля</Typo></Typo>
+			</div>
+
 			<Button element="a"
 			        onClick={() => history.push('/cafes')}
 			        full
