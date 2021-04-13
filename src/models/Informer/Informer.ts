5afe2e5b8a8a7ceebc1484f015d3bc47ef831e90
Arthur <aptyp.potatoch@gmail.com>
2021-04-14 01:23:19 +0300

    informers frames added

---
+++ src/models/Informer/Informer.ts
@@ -0,0 +1,9 @@
+export default class Informer {
+	constructor(
+		public title: string,
+		public text: string,
+		public image: string,
+		public type: number,
+	) {
+	}
+}
---
+++ src/models/Informer/InformerBucket.ts
@@ -0,0 +1,34 @@
+import Informer from '@models/Informer/Informer';
+import Http from '@network/Http';
+
+export default class InformerBucket {
+	informerBucket: Informer[];
+
+	async fetchInformers(): Promise<Informer[]> {
+		return Http.getInformers()
+			.then((response: Response | null) => {
+				if (response === null) return [];
+				if (!response.ok) {
+					console.log('informer request is not ok');
+					console.log(`inf request status = ${response.status}`);
+					console.log(`inf request status text = ${response.statusText}`);
+					return [];
+				}
+
+				return response.json();
+			});
+	}
+
+	getInformer(): Informer | null {
+		if (this.informerBucket.length === 0) {
+			this.fetchInformers()
+				.then((informers) => {
+					this.informerBucket = informers;
+				});
+
+			return null;
+		}
+
+		return this.informerBucket.pop();
+	}
+}
