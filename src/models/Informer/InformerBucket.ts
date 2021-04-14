35cd98eb70f635c47ba0da1d7325b7cf19a73ea9
Arthur <aptyp.potatoch@gmail.com>
2021-04-14 10:58:55 +0300

    fetch informers improved

--- src/models/Informer/InformerBucket.ts
+++ src/models/Informer/InformerBucket.ts
@@ -4,8 +4,8 @@ import Http from '@network/Http';
 class InformerBucket {
 	informerBucket: Informer[];
 
-	async fetchInformers(): Promise<Informer[]> {
-		return Http.getInformers()
+	async fetchInformers(): Promise<void> {
+		this.informerBucket = await Http.getInformers()
 			.then((response: Response | null) => {
 				if (response === null) return [];
 				if (!response.ok) {
@@ -21,11 +21,7 @@ class InformerBucket {
 
 	getInformer(): Informer | null {
 		if (this.informerBucket.length === 0) {
-			this.fetchInformers()
-				.then((informers) => {
-					this.informerBucket = informers;
-				});
-
+			this.fetchInformers();
 			return null;
 		}
 
