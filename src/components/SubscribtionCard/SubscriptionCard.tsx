fa338d3739145f617019a17e6da819a17f7a415d
Arthur <aptyp.potatoch@gmail.com>
2021-04-18 19:51:24 +0300

    margin improved

--- src/components/SubscribtionCard/SubscriptionCard.tsx
+++ src/components/SubscribtionCard/SubscriptionCard.tsx
@@ -37,87 +37,90 @@ const SubscriptionCard: React.FC = () => {
 	}, []);
 
 	return (
-		<div className="card">
-			<Typo color={TypoColor.white} type={TypographyType.h3} textAlign={TypoTextAlign.start} block
-			      style={{lineHeight: '24px', marginTop: 24, fontWeight: 500}}
-			>
-				Подписка на сервиc
-			</Typo>
-			<div className="card__features">
-				<Typo
-					color={TypoColor.white}
-					icon={<CupIcon color={IconColor.white} className="card__icon"
-					               size={IconSize.xs}/>}
-					type={TypographyType.h4}
+		<div className="card-container">
+			<div className="card">
+				<Typo color={TypoColor.white} type={TypographyType.h3} textAlign={TypoTextAlign.start}
+				      block
+				      style={{lineHeight: '24px', marginTop: 24, fontWeight: 500}}
 				>
-					Безлимитный чай и кофе
+					Подписка на сервиc
 				</Typo>
-				<Typo
-					color={TypoColor.white}
-					icon={<WorkIcon color={IconColor.white} className="card__icon"
-					                size={IconSize.xs}/>}
-					type={TypographyType.h4}
-				>
-					Неограниченный доступ в рабочие пространства
-				</Typo>
-				<Typo
-					color={TypoColor.white}
-					icon={<LaptopIcon color={IconColor.white} className="card__icon"
-					                  size={IconSize.xs}/>}
-					type={TypographyType.h4}>
-					Бесконечная продуктивность
-				</Typo>
-				<Typo
-					color={TypoColor.white}
-					icon={<CheckIcon color={IconColor.white} className="card__icon"
-					                 size={IconSize.xs}/>}
-					type={TypographyType.h4}
-				>
-					И много другого
-				</Typo>
-
-				<Separator style={{backgroundColor: 'white', marginTop: 24}}/>
-
-				<div style={{display: 'flex', justifyContent: 'space-between'}}>
-					<Typo type={TypographyType.h4} color={TypoColor.white}>
-						Ранний доступ
+				<div className="card__features">
+					<Typo
+						color={TypoColor.white}
+						icon={<CupIcon color={IconColor.white} className="card__icon"
+						               size={IconSize.xs}/>}
+						type={TypographyType.h4}
+					>
+						Безлимитный чай и кофе
 					</Typo>
+					<Typo
+						color={TypoColor.white}
+						icon={<WorkIcon color={IconColor.white} className="card__icon"
+						                size={IconSize.xs}/>}
+						type={TypographyType.h4}
+					>
+						Неограниченный доступ в рабочие пространства
+					</Typo>
+					<Typo
+						color={TypoColor.white}
+						icon={<LaptopIcon color={IconColor.white} className="card__icon"
+						                  size={IconSize.xs}/>}
+						type={TypographyType.h4}>
+						Бесконечная продуктивность
+					</Typo>
+					<Typo
+						color={TypoColor.white}
+						icon={<CheckIcon color={IconColor.white} className="card__icon"
+						                 size={IconSize.xs}/>}
+						type={TypographyType.h4}
+					>
+						И много другого
+					</Typo>
+
+					<Separator style={{backgroundColor: 'white', marginTop: 24}}/>
+
+					<div style={{display: 'flex', justifyContent: 'space-between'}}>
+						<Typo type={TypographyType.h4} color={TypoColor.white}>
+							Ранний доступ
+						</Typo>
+
+						<Typo
+							block
+							type={TypographyType.h4}
+							color={TypoColor.white}
+							style={{textDecoration: 'line-through'}}
+						>
+							5000 руб / мес
+						</Typo>
+					</div>
 
 					<Typo
-						block
-						type={TypographyType.h4}
 						color={TypoColor.white}
-						style={{textDecoration: 'line-through'}}
+						block
+						className="card__price-old"
+						type={TypographyType.h3}
+						textAlign={TypoTextAlign.end}
+						style={{lineHeight: '24px', fontWeight: 500}}
 					>
-						5000 руб / мес
+						3900 руб / мес
 					</Typo>
+
+					<Button
+						disabled={!url}
+						element={'a'}
+						href={url}
+						buttonSize={ButtonSize.xl}
+						full
+						onClick={() => {
+							setPayOffer();
+							ym('reachGoal', 'Переход в yookassa');
+						}}
+						style={{margin: '13px 0'}}
+					>
+						{t(Phrase.pay)}
+					</Button>
 				</div>
-
-				<Typo
-					color={TypoColor.white}
-					block
-					className="card__price-old"
-					type={TypographyType.h3}
-					textAlign={TypoTextAlign.end}
-					style={{lineHeight: '24px', fontWeight: 500}}
-				>
-					3900 руб / мес
-				</Typo>
-
-				<Button
-					disabled={!url}
-					element={'a'}
-					href={url}
-					buttonSize={ButtonSize.xl}
-					full
-					onClick={() => {
-						setPayOffer();
-						ym('reachGoal','Переход в yookassa');
-					}}
-					style={{margin: '13px 0'}}
-				>
-					{t(Phrase.pay)}
-				</Button>
 			</div>
 		</div>
 	);
