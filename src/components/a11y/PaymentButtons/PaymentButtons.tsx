1c51174a4a10f9b04e94775cc3f056783ca582d8
Sergey Petrenko <sergey.petrenko@B-5BXWMD6M-0155.local>
2021-04-27 21:12:29 +0300

    fix

--- src/components/a11y/PaymentButtons/PaymentButtons.tsx
+++ src/components/a11y/PaymentButtons/PaymentButtons.tsx
@@ -6,24 +6,47 @@ import t, {Phrase} from '@models/Translate';
 import {observer} from 'mobx-react-lite';
 import ym from 'react-yandex-metrika';
 import {setPayOffer} from '@utils/payStorage';
+import Message from '@models/Notification/Notification';
 
 type Props = {
 	dark?: boolean;
 }
+function openTab(url) {
+	// Create link in memory
+	const a = window.document.createElement('a');
+	a.target = '_blank';
+	a.href = url;
+
+	// Dispatch fake click
+	const e = window.document.createEvent('MouseEvents');
+	e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+	a.dispatchEvent(e);
+}
+
 const PaymentButtons: React.FC<Props> = ({dark = false}) => {
 	const history = useHistory();
 
 	const [url, setUrl] = React.useState<string | null>(null);
-	React.useEffect(() => {
+	const getUrl = React.useCallback(() => {
+		const now = new Date();
+		now.setHours(0);
+		now.setMinutes(0);
+		now.setSeconds(0);
 		Http.fetchPost({
 			path: '/payments/',
-			body: null,
+			body: {
+				'subscription': 2,
+				'subscribe_begin': now.toISOString()
+			},
 		})
 			.then((r) => {
 				r.json().then((data) => {
 					try {
 						if (data?.url) {
-							setUrl(data?.url);
+							openTab(data.url);
+							// setUrl(data?.url);
+						} else{
+							Message.error('Сервис недоступен. Попробуйте еще раз.');
 						}
 					} catch (e) {
 						console.log(`subscription main error: ${e}`);
@@ -36,14 +59,15 @@ const PaymentButtons: React.FC<Props> = ({dark = false}) => {
 
 	return <div>
 		<Button
-			disabled={!url}
-			element={'a'}
-			href={url}
+			// disabled={!url}
+			// element={'a'}
+			// href={url}
 			buttonSize={ButtonSize.xl}
 			full
 			onClick={() => {
 				ym('reachGoal', 'Переход в yookassa');
 				setPayOffer();
+				getUrl();
 			}}
 			style={{margin: '13px 0'}}
 		>
