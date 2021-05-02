6ef79336208fbeb5abc600ad83c4d74f8c2f7473
Sergey Petrenko <sergey.petrenko@B-5BXWMD6M-0155.local>
2021-05-02 18:27:22 +0300

    promo api

--- src/pages/CafePage/CafePage.tsx
+++ src/pages/CafePage/CafePage.tsx
@@ -8,41 +8,70 @@ import {BackIcon, IconSize} from '@components/primitives/Icon';
 import DetailedInfo from '@components/DetailedInfo';
 import {observer} from 'mobx-react-lite';
 import PromoCard from '@components/PromoCard';
-import {PromoCardColor} from '@components/PromoCard/PromoCard';
+import {PromoCardColor, PromoCardProps} from '@components/PromoCard/PromoCard';
+
+const normalizePlace = (data: any):CafeCardProps =>({
+	id: data.id,
+	imageSrc: data['main_image'] || (data['images'] && data['images'][0]),
+	name: data['full_name'] || data['short_name'],
+	statuses: data.categories,
+	averagePrice: data['average_bill'],
+	workLoadText: data.occupancy,
+	address: data.address,
+	wifi: data.wifi,
+	images: data['images'],
+	electricity: data['power_socket'],
+	quiet: data.silence,
+	light: data.light,
+	time: data['opening_hours'] && data['opening_hours']['open_time'] && data['opening_hours']['close_time'] && `${data['opening_hours']['open_time']} - ${data['opening_hours']['close_time']}`,
+	workLoad: data['work_places'],
+	mapSrc: (data.address || data['full_name'] || data['short_name']) && `https://yandex.ru/maps/213/moscow/search/${data.address || data['full_name'] || data['short_name']}`,
+});
+
+const normalizePromotions = (data:any[]):PromoCardProps[] => {
+	if(!data || !data.length){
+		return [];
+	}
+	console.log('norm', data);
+	return data.map((data:any):PromoCardProps => ({
+		img: data.image,
+		title: data.title,
+		subtitle: data['discount_value']
+	}));
+};
 
 const CafePage: React.FC = () => {
 	const {id} = useParams<{ id }>();
 
 	const [cafe, setCafe] = React.useState<CafeCardProps>();
+	const [promotions, setPromotions] = React.useState<PromoCardProps[]>();
 
 	React.useEffect(() => {
-		Http.fetchGet({
-			path: `/places/${id}/`,
-		})
-			.then((r) => {
-				r.json().then(((data) => {
-					console.log('data', data);
-					setCafe(
-						{
-							id: data.id,
-							imageSrc: data['main_image'] || (data['images'] && data['images'][0]),
-							name: data['full_name'] || data['short_name'],
-							statuses: data.categories,
-							averagePrice: data['average_bill'],
-							workLoadText: data.occupancy,
-							address: data.address,
-							wifi: data.wifi,
-							images: data['images'],
-							electricity: data['power_socket'],
-							quiet: data.silence,
-							light: data.light,
-							time: data['opening_hours'] && data['opening_hours']['open_time'] && data['opening_hours']['close_time'] && `${data['opening_hours']['open_time']} - ${data['opening_hours']['close_time']}`,
-							workLoad: data['work_places'],
-							mapSrc: (data.address || data['full_name'] || data['short_name']) && `https://yandex.ru/maps/213/moscow/search/${data.address || data['full_name'] || data['short_name']}`,
-						});
-				}));
+		Promise.all([
+			Http.fetchGet({
+				path: `/places/${id}/`,
 			})
-			.catch(console.log);
+				.then((r) => {
+					r.json().then(((data) => {
+						setCafe(normalizePlace(data));
+					}));
+				})
+				.catch(console.log),
+
+			Http.fetchGet({
+				path: `/places/${id}/promotions`,
+			})
+				.then((r) => {
+					r.json().then(((data) => {
+						if(!data || !data.length){
+							return;
+						}
+						console.log('tetete',{data});
+						setPromotions(normalizePromotions(data));
+					}));
+				})
+				.catch(console.log)
+		]);
 	}, []);
 
 	const history = useHistory();
@@ -51,7 +80,7 @@ const CafePage: React.FC = () => {
 		mainProps={{
 			body: () => <>
 				<div className="cafe-page">
-					{cafe && <DetailedInfo {...cafe} />}
+					{cafe && <DetailedInfo {...cafe} promotions={promotions} />}
 				</div>
 			</>,
 		}}/>);
