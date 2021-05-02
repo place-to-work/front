6eb1141395d59136bf37d8703e04849c923d3567
Arthur <aptyp.potatoch@gmail.com>
2021-05-02 19:08:17 +0300

    popup frame totally done

--- src/components/QrPopup/QrPopup.tsx
+++ src/components/QrPopup/QrPopup.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import './QrPopup.scss';
+import CrossIcon from '@components/primitives/Icon/components/CrossIcon';
+import {IconColor} from '@components/primitives/Icon';
+import QRCode from 'qrcode.react';
+import Typo, {TypographyType, TypoTextAlign} from '@components/primitives/Typo';
 
 interface OwnProps {
 	link?: string;
+	setLink?: (link?: string) => void;
 }
 
-const QrPopup: React.FC<OwnProps> = ({}) => <div className="qr-popup__background">
-	<div className="qr-popup">
+const QrPopup: React.FC<OwnProps> = ({link = 'qwer', setLink}) => {
+	if (!link) {
+		return null;
+	}
 
-	</div>
-</div>;
+	return <div className="qr-popup__background">
+		<div className="qr-popup">
+			<CrossIcon
+				style={{position: 'absolute', right: 18, top: 22}}
+				color={IconColor.black}
+				onClick={() => {
+					console.log('cross icon clicked');
+					if (setLink) {
+						setLink(undefined);
+					}
+				}}
+			/>
+			<div className="qr-popup__contents">
+				<QRCode
+					className="qr-popup__qr"
+					renderAs="svg"
+					value={link}
+				/>
+				<Typo type={TypographyType.h4} style={{width: '80%', marginTop: 30}} textAlign={TypoTextAlign.center}>
+					Чтобы получить скидку, покажите QR-код бариста
+				</Typo>
+			</div>
+		</div>
+	</div>;
+};
 
 export default QrPopup;
--- src/components/primitives/Icon/components/CrossIcon.tsx
+++ src/components/primitives/Icon/components/CrossIcon.tsx
@@ -3,8 +3,8 @@ import BaseIcon, {BaseIconProps} from './BaseIcon';
 
 const CrossIcon: React.FC<BaseIconProps> = (props: BaseIconProps) => (
 	<BaseIcon {...props} width="24" height="24" viewBox="0 0 24 24">
-		<path d="M20 2L2 20" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-		<path d="M2 2L20 20" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+		<path d="M20 2L2 20" stroke={props.color} strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" fill="currentColor"/>
+		<path d="M2 2L20 20" stroke={props.color} strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" fill="currentColor"/>
 	</BaseIcon>
 );
 
