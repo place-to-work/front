819c0f8fb597c2cf673dd9f05be0aeb458b58eb3
Sergey Petrenko <sergey.petrenko@B-5BXWMD6M-0155.local>
2021-05-16 12:57:07 +0300

    promo fix

--- src/components/DetailedInfo/DetailedInfo.tsx
+++ src/components/DetailedInfo/DetailedInfo.tsx
@@ -202,13 +202,12 @@ const DetailedInfo: React.FC<CafeCardProps> = (
 										if(promo.type === 1 && !User.isSubscribed){
 											console.log('REDIRECT');
 											history.push('/in-place');
-										}
-										if ((User.isSubscribed || promo.type === 0 ) && uuid ) {
+										} else if ((User.isSubscribed || promo.type === 0 ) && uuid ) {
 											console.log('PROMO MODAL');
 											console.log(`det inf uuid: ${uuid}`);
 											console.log(`det inf promotion: ${promo.id}`);
 											setLink(`${Http.serverUrl}/success-discount?uuid=${uuid.subscribe_uuid}&promotion=${promo.id}`);
-										}else {
+										} else {
 											Notification.error('ошибка получения uuid');
 										}
 									});
