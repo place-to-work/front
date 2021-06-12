94b580760e236b44389be4fe60e1fd275b230cb4
Arthur <aptyp.potatoch@gmail.com>
2021-06-12 19:23:04 +0300

    button is centralized now

--- src/pages/CafeMap/CafeListButton.tsx
+++ src/pages/CafeMap/CafeListButton.tsx
@@ -3,20 +3,20 @@ import Button from '@components/primitives/Button';
 
 
 const CafeListButton: React.FC = () => {
-	return <Button
-		style={{
-			width: 173,
-			height: 39,
-			backgroundColor: 'black',
-			borderRadius: 8,
-			position: 'absolute',
-			bottom: 10,
-			margin: '0 auto',
-		}}
-		href={'/places/'}
-	>
-		Списком
-	</Button>;
+	return <div style={{width: '100vw', position: 'absolute', bottom: 10}}>
+		<Button
+			style={{
+				width: 173,
+				height: 39,
+				backgroundColor: 'black',
+				borderRadius: 8,
+				margin: '0 auto',
+			}}
+			href={'/places/'}
+		>
+			Списком
+		</Button>
+	</div>;
 }
 
 export default CafeListButton;
